cc76ff7a238015b2a69e22cd244ac6f8f01cba6d
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-14 22:02:48 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,9 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  passengers.each do |name|
-    puts "#{name}"
-  end
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+end
 end
